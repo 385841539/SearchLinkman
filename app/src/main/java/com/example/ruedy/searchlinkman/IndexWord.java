d6e0eea391ffb34c539c7c520051a8e9c673d1be
385841539 <385841539@qq.com>
2017-03-13 20:16:13 +0800

    完善功能

--- app/src/main/java/com/example/ruedy/searchlinkman/IndexWord.java
+++ app/src/main/java/com/example/ruedy/searchlinkman/IndexWord.java
@@ -42,7 +42,7 @@ public class IndexWord extends View {
 
     }
 
-    private void initpaint() {
+    private void initpaint() {//初始化文本画笔
         textPaint = new TextPaint(Paint.ANTI_ALIAS_FLAG);
         textPaint.setColor(Color.BLACK);
         textPaint.setTextSize(30);
@@ -53,14 +53,16 @@ public class IndexWord extends View {
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN|MotionEvent.ACTION_MOVE:
+            case MotionEvent.ACTION_DOWN | MotionEvent.ACTION_MOVE:
+                //如果是点击或者滑动状态，则使字幕变色，并回调实现方法
                 index = (int) (event.getY() / everyheght);
+                //获取被点击的字幕，然后回调出去
                 if (indexPressWord != null && index >= 0) {
                     indexPressWord.setIndexPressWord(words[index]);
                 }
-                invalidate();
+                invalidate();//重走onDraw方法。
                 break;
-            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_UP://手指抬起，所有字母变为黑色
                 index = -1;
                 invalidate();
                 break;
@@ -78,12 +80,12 @@ public class IndexWord extends View {
             textPaint.getTextBounds(word, 0, 1, rect);
             int width = rect.width();
             int height = rect.height();
-            if (index == i) {
+            if (index == i) {//被点击的字幕为红色
                 textPaint.setColor(Color.RED);
             } else {
                 textPaint.setColor(Color.BLACK);
             }
-            canvas.drawText(word, everywidth / 2 - width / 2, everyheght * i+(everyheght/2+ height / 2), textPaint);
+            canvas.drawText(word, everywidth / 2 - width / 2, everyheght * i + (everyheght / 2 + height / 2), textPaint);
         }
 
     }
@@ -94,6 +96,9 @@ public class IndexWord extends View {
         everyheght = getHeight() / length;
     }
 
+    /**
+     * 接口，用于回调点击和滑动事件
+     */
     public interface IndexPressWord {
         void setIndexPressWord(String word);
     }
